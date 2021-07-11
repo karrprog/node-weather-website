6db6492c66a4e84de0446ba3471ed29d15b4786e
Nathaniel Karr <41494597+karrprog@users.noreply.github.com>
2021-07-11 12:43:23 -0500

    Updating forecast message to be more informative

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location, please try another');
         } else{
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + ' degrees')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + 
+            ' degrees out. Current humidity is ' + body.current.humidity + '%. Wind direction: ' + body.current.wind_dir + ' @ ' + body.current.wind_speed + ' MPH');
         }
     })
 }
